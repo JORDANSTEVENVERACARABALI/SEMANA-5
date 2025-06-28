3f79282bc2227d35ca18644f34381054a365d06e
JORDANSTEVENVERACARABALI <159176328+JORDANSTEVENVERACARABALI@users.noreply.github.com>
2025-06-28 11:21:31 -0500

    Delete SEMANA 5 directory

--- SEMANA 5/SEMANA_5.py
+++
@@ -1,6 +0,0 @@
-# Lista de asignaturas
-asignaturas = ["Matemáticas", "Física", "Química", "Historia", "Lengua"]
-
-# Mostrar cada asignatura con el mensaje correspondiente
-for asignatura in asignaturas:
-    print(f"Yo estudio {asignatura}")
