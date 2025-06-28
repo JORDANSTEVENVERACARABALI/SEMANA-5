5ab6ce7cc895cdf7b9a74e6741b24160747ed1d2
CF <CF@DESKTOP-DQ57TL8>
2025-06-28 11:05:25 -0500

    Agregar archivos de proyecto.

---
+++ SEMANA 5/SEMANA_5.py
@@ -0,0 +1,6 @@
+# Lista de asignaturas
+asignaturas = ["Matemáticas", "Física", "Química", "Historia", "Lengua"]
+
+# Mostrar cada asignatura con el mensaje correspondiente
+for asignatura in asignaturas:
+    print(f"Yo estudio {asignatura}")
